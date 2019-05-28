e87ac3be9795be8cb1d47b1959df140e5721607f
minueliz <47104642+minueliz@users.noreply.github.com>
2019-05-28 11:47:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body background="1.jpg">
+<center> 
+<table bgcolor=white border=0 width=100% height=6%>
+<tr><td>
+<table>
+</table>
+<td><table bgcolor=white border=0 width=80%>
+<tr><td align=center width=30% font color=red size=5><a href=index.php  style="color:red; text-decoration:none;"><b>Home</b>  
+<td align=center width=30% font color=red size=5><a href=aboutas.php    style="color:red; text-decoration:none;"><b>Aboutus</b>
+<td align=center width=30% font color=red size=5><a href=registration.php   style="color:red; text-decoration:none;"><b>Register</b>
+<td align=center width=30% font color=red size=5><a href=login.php    style="color:red; text-decoration:none;"><b>Login</b>
+</table>
+</table>
+</html>
